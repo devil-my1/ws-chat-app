04b5327de6f107d121cd5375fad8807a34760396
Fallen <amir.amon@mail.ru>
2024-05-07 20:14:20 +0900

    remove unsed files

--- src/config/aws-config.ts
+++ src/config/aws-config.ts
@@ -1,5 +1,5 @@
 export const awsConfig = {
 	region: "ap-northeast-1",
-	USER_POOL_ID: "ap-northeast-1_ATo4apdEL",
-	USER_POOL_CLIENT_ID: "3vsu9jjeh1pn51c552gpvq87ap"
+	USER_POOL_ID: "ap-northeast-1_gmXyyR9hB",
+	USER_POOL_CLIENT_ID: "160u5it96igidgvevuh6hmpkpl"
 }
